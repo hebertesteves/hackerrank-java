2bbe512a736082ad3d41c0db530d1aacba736416
Hebert Esteves <108402431+hebertesteves@users.noreply.github.com>
2025-08-13 18:07:19 -0300

    feat: adiciona solução para Ex02_JavaStdinAndStdoutI

---
+++ src/easy/Ex02_JavaStdinAndStdoutI.java
@@ -0,0 +1,18 @@
+package easy;
+
+import java.util.Scanner;
+
+public class Ex02_JavaStdinAndStdoutI {
+    // O nome da classe no HackerRank é Solution
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
