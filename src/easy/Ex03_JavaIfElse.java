b729182e3e4ec0adca23536ef5074090180af281
Hebert Esteves <108402431+hebertesteves@users.noreply.github.com>
2025-08-13 18:07:28 -0300

    feat: adiciona solução para Ex03_JavaIfElse

---
+++ src/easy/Ex03_JavaIfElse.java
@@ -0,0 +1,34 @@
+package easy;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Ex03_JavaIfElse {
+    // O nome da classe no HackerRank é Solution
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if (N % 2 != 0) {
+            System.out.println("Weird");
+        } else {
+            if (N >= 2 && N <= 5) {
+                System.out.println("Not Weird");
+            } else if (N > 5 && N <= 20) {
+                System.out.println("Weird");
+            } else if (N > 20) {
+                System.out.println("Not Weird");
+            }
+        }
+
+        scanner.close();
+    }
+}
