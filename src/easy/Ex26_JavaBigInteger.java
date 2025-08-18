256d84b37b3dfb1a9490881812e53db0968939c6
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-08-18 19:32:32 -0300

    feat: adiciona solução para Ex26_JavaBigInteger

---
+++ src/easy/Ex26_JavaBigInteger.java
@@ -0,0 +1,21 @@
+package easy;
+
+import java.util.*;
+import java.math.*;
+
+public class Ex26_JavaBigInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+
+        BigInteger product =  a.multiply(b);
+        BigInteger sum = a.add(b);
+
+        System.out.println(sum);
+        System.out.println(product);
+
+        sc.close();
+    }
+}
