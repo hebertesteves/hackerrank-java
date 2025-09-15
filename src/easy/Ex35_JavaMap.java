767e49848a321614db210350f7201ec0604533e4
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-09-14 21:03:37 -0300

    feat: adiciona solução para Ex35_JavaMap

---
+++ src/easy/Ex35_JavaMap.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.*;
+import java.io.*;
+
+public class Ex35_JavaMap {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Map<String, Integer> phoneBook = new HashMap<>();
+        int n=in.nextInt();
+        in.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            phoneBook.put(name, phone);
+            in.nextLine();
+        }
+        while(in.hasNext())
+        {
+            String s=in.nextLine();
+            if (phoneBook.containsKey(s)) {
+                System.out.println(s + "=" + phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+    }
+}
