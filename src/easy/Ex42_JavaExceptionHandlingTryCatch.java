37bf90eaa89e0cb158767f7ed056bbada55f79ff
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-09-30 18:56:05 -0300

    feat: adiciona solução para Ex42_JavaExceptionHandlingTryCatch

---
+++ src/easy/Ex42_JavaExceptionHandlingTryCatch.java
@@ -0,0 +1,23 @@
+package easy;
+
+import java.io.*;
+import java.util.*;
+
+public class Ex42_JavaExceptionHandlingTryCatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            int resultado = x / y;
+            System.out.println(resultado);
+        } catch (InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        } catch (ArithmeticException e) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        } finally {
+            sc.close();
+        }
+    }
+}
